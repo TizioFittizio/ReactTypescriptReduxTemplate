fdc5b6c9eb3f2f149f981ad67a1406968a82bbeb
C_Minor <luca.campa.94@gmail.com>
2018-04-17 22:43:08 +0200

    Minor changes

--- src/reducers/Reducers.tsx
+++ src/reducers/Reducers.tsx
@@ -5,13 +5,10 @@ import { SHUFFLE, CHANGE_FONT, FETCH_DATA } from '../constants';
 export function reducer(state: StoreState, action: ContainerAction): StoreState {
   switch (action.type) {
     case CHANGE_FONT:
-        console.log(state);
         return {...state, font: action.payload};
     case SHUFFLE:
-        console.log(state);
         return {...state, phrase: action.payload};
     case FETCH_DATA:
-        console.log(state);
         alert(`Data fetched!\n${action.payload.data.title}`);
         return {...state};
     default:
